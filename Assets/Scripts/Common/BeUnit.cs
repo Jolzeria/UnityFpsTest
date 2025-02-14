c40daf7f9a2db9e4c3e7353427c90ed7b2add1de
果冻 <jellylno@gmail.com>
2025-02-14 21:00:08 +0800

    碰撞事件放到EnemyUnit中

--- Assets/Scripts/Common/BeUnit.cs
+++ Assets/Scripts/Common/BeUnit.cs
@@ -29,47 +29,10 @@ public class BeUnit : MonoBehaviour
 
     protected virtual void RegisterEvent()
     {
-        EventHandler.RegisterEvent<DamageInfo>(this, GameEventEnum.DamageProcess, DamageProcess);
     }
 
     protected virtual void UnRegisterEvent()
     {
-        EventHandler.UnRegisterEvent<DamageInfo>(this, GameEventEnum.DamageProcess, DamageProcess);
-    }
-
-    private void DamageProcess(DamageInfo damageInfo)
-    {
-        Debug.Log("player ATK:" + damageInfo.snapShot.GetAttrValue(AttributeType.ATK));
-        Debug.Log("receiver DEF:" + damageInfo.receiver.GetAttrValue(AttributeType.DEF));
-        float addHp = damageInfo.receiver.GetAttrValue(AttributeType.DEF) -
-                      damageInfo.snapShot.GetAttrValue(AttributeType.ATK);
-        if (addHp > 0)
-            addHp = 0f;
-
-        float curHp = damageInfo.receiver.GetAttrValue(AttributeType.CurHp);
-        if (curHp <= 0)
-        {
-            Debug.Log("敌人已死亡");
-            return;
-        }
-
-        if (Mathf.Abs(addHp) >= curHp)
-        {
-            addHp = -curHp;
-            
-            // 击杀获得分数
-            ScoreManager.Instance.Score3Add(1);
-            Debug.Log("击败敌人");
-        }
-
-        // 击中获得分数
-        ScoreManager.Instance.Score2Add(1);
-        
-        damageInfo.receiver.AddAttrValue(AttributeType.CurHp, addHp);
-        Debug.Log("receiver CurHp:" + damageInfo.receiver.GetAttrValue(AttributeType.CurHp));
-
-        DamageTextManager.Instance.Add(new DamageTextManager.DamageTextData()
-            {position = damageInfo.hitPoint, damage = Mathf.RoundToInt(addHp)});
     }
 
     public void AddAttrValue(AttributeType attr, float value)
@@ -81,7 +44,7 @@ public class BeUnit : MonoBehaviour
     {
         return attribute.GetAttrValue(attr);
     }
-    
+
     public void SetAttrValue(AttributeType attr, float value)
     {
         attribute.SetAttrValue(attr, value);
--- Assets/Scripts/Enemy/EnemyUnit.cs
+++ Assets/Scripts/Enemy/EnemyUnit.cs
@@ -18,4 +18,56 @@ public class EnemyUnit : BeUnit
 
         attribute.UnInit();
     }
+
+    protected override void RegisterEvent()
+    {
+        base.RegisterEvent();
+        
+        EventHandler.RegisterEvent<DamageInfo>(this, GameEventEnum.DamageProcess, DamageProcess);
+    }
+
+    protected override void UnRegisterEvent()
+    {
+        base.UnRegisterEvent();
+        
+        EventHandler.UnRegisterEvent<DamageInfo>(this, GameEventEnum.DamageProcess, DamageProcess);
+    }
+    
+    private void DamageProcess(DamageInfo damageInfo)
+    {
+        Debug.Log("player ATK:" + damageInfo.snapShot.GetAttrValue(AttributeType.ATK));
+        Debug.Log("receiver DEF:" + damageInfo.receiver.GetAttrValue(AttributeType.DEF));
+        float addHp = damageInfo.receiver.GetAttrValue(AttributeType.DEF) -
+                      damageInfo.snapShot.GetAttrValue(AttributeType.ATK);
+        if (addHp > 0)
+            addHp = 0f;
+
+        float curHp = damageInfo.receiver.GetAttrValue(AttributeType.CurHp);
+        if (curHp <= 0)
+        {
+            Debug.Log("敌人已死亡");
+            return;
+        }
+
+        if (Mathf.Abs(addHp) >= curHp)
+        {
+            addHp = -curHp;
+            
+            // 击杀获得分数
+            ScoreManager.Instance.Score3Add(1);
+            Debug.Log("击败敌人");
+            
+            // 自毁
+            Destroy(gameObject);
+        }
+
+        // 击中获得分数
+        ScoreManager.Instance.Score2Add(1);
+        
+        damageInfo.receiver.AddAttrValue(AttributeType.CurHp, addHp);
+        Debug.Log("receiver CurHp:" + damageInfo.receiver.GetAttrValue(AttributeType.CurHp));
+
+        DamageTextManager.Instance.Add(new DamageTextManager.DamageTextData()
+            {position = damageInfo.hitPoint, damage = Mathf.RoundToInt(addHp)});
+    }
 }
